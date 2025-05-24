abc552f74dda3bddb62b50fc9ced22df7e65354d
tsumugivolle77 <endsieg77@gmail.com>
2025-05-25 01:34:10 +0200

    add dpa3 nr 3 5

---
+++ dpa2/marie/build/CMakeFiles/prog.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prog.dir/task03.c.o"
+  "CMakeFiles/prog.dir/task03.c.o.d"
+  "prog"
+  "prog.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/prog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
