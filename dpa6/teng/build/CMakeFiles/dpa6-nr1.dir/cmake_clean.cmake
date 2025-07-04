2a3f56eac9b056cb56164294a7e06640f9261ad7
tsumugivolle77 <endsieg77@gmail.com>
2025-07-04 23:28:32 +0200

    add sheet6/1,3

---
+++ dpa6/teng/build/CMakeFiles/dpa6-nr1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dpa6-nr1.dir/p1.c.o"
+  "CMakeFiles/dpa6-nr1.dir/p1.c.o.d"
+  "dpa6-nr1"
+  "dpa6-nr1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dpa6-nr1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
